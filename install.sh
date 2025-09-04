f60fde3979381b3fb68efc0ed22fbd3a691c15bb
Socialoutcast <socialoutcast@3cn3t.com>
2025-09-04 09:48:01 -0700

    fix: update minimal installer to use correct script name

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Spotify Kids Manager - Installer
-curl -sL https://github.com/socialoutcast/spotify-kids-manager/releases/latest/download/installer-scripts.tar.gz | tar xz -C /tmp && sudo /tmp/install.sh
+curl -sL https://github.com/socialoutcast/spotify-kids-manager/releases/latest/download/installer-scripts.tar.gz | sudo tar xz -C /tmp && sudo bash /tmp/installer-full.sh
