13e88fc5141a25914f97fdfced8947fc2c388785
Socialoutcast <socialoutcast@3cn3t.com>
2025-09-05 09:18:56 -0700

    chore: update setup.sh for v0.2.2 release

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ set -e
 
 # Encrypted password for the release archive
 # This is decrypted at runtime using a known key
-ENCRYPTED_PASSWORD="U2FsdGVkX1/KVYy9JmLadlX/NXZ3/x8224vKdqNtYNxmZrSO4nRDlU/CNAgFTIXZA6FmFBdTfBMpAU6BbR/9LA=="
+ENCRYPTED_PASSWORD="U2FsdGVkX1+8aMvhn17VL6hI4sgE5wayaIGn/l5xcss3+p3JUtePTHcvIVYVVIfyDjw1HRVEihFUZdMCsSODug=="
 
 # Decrypt the password using standard Linux tools
 # Use pbkdf2 for OpenSSL 3.0+ (matching encryption)
